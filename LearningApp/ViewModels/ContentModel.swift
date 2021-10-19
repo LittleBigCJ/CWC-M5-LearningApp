a8b92e68663a42ce9b92929f1166456cbc39d382
Chris Houghton <christopherjhoughton2@gmail.com>
2021-10-19 17:15:18 +0100

    -organise files into folders -add content model -set content model as environment object -rename contentView

---
+++ LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,14 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by Chris Houghton on 19/10/2021.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+    
+    
+}
