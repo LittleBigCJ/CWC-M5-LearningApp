c7395efa1005eb00260730becade35f8709ee0d2
Chris Houghton <christopherjhoughton2@gmail.com>
2021-10-19 17:49:29 +0100

    -add assets -add data files -add models -parse json in content model -parse style data in content model

---
+++ LearningApp/Models/Models.swift
@@ -0,0 +1,48 @@
+//
+//  Models.swift
+//  LearningApp
+//
+//  Created by Chris Houghton on 19/10/2021.
+//
+
+import Foundation
+
+struct Module: Decodable, Identifiable {
+    
+    var id: Int
+    var category: String
+    var content: Content
+    var test: Test
+}
+struct Content: Decodable, Identifiable {
+    
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var lessons: [Lesson]
+}
+
+struct Lesson: Decodable, Identifiable {
+    var id: Int
+    var title: String
+    var video: String
+    var duration: String
+    var explanation: String
+}
+
+struct Test: Decodable, Identifiable {
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var questions: [Question]
+}
+
+struct Question: Decodable, Identifiable {
+    var id: Int
+    var content: String
+    var correctIndex: Int
+    var answers: [String]
+   
+}
--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -6,9 +6,56 @@
 //
 
 import Foundation
+import SwiftUI
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    var styleData: Data?
     
+    init(){
+        getLocalData()
+        
+    }
+    
+    func getLocalData(){
+        //get a url to the json file
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        //read the file into a data object
+        do {
+            //try to decode the json into an array of modules
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            let jsonDecoder = JSONDecoder()
+            
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            //assign parsed modules to modules property
+            self.modules = modules
+            
+        }
+        catch {
+            //log error
+            print("Couldn't parse local data")
+        }
+        //parse the style data
+        //get a url to the json file
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        //read the file into a data object
+        do {
+            //try to decode the json into an array of modules
+            let styleData = try Data(contentsOf: styleUrl!)
+            self.styleData = styleData
+            
+        }
+        catch {
+            
+            print("Coudn't parse style data")
+            
+            
+        }
+        
+    }
     
 }
+
+
+
